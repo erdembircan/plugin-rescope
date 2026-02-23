dacceb911d57774ba6c052ae0e0593eb2f910f0e
Erdem Bircan <erdem.erdembircan@gmail.com>
2026-02-23 15:56:37 +0300

    [CHORE]: integrate Prettier into ESLint via eslint-plugin-prettier
    - Add eslint-plugin-prettier and eslint-config-prettier as dev dependencies
    - Update eslint.config.js to use eslint-plugin-prettier/recommended config
    - Move Prettier options from .prettierrc inline to ESLint rule config
    - Remove standalone .prettierrc and .prettierignore files
    - Replace format/format:check scripts with lint:fix script

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,26 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettier,
   {
     ignores: ["build/", "node_modules/", "vitest.config.ts"],
+  },
+  {
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          semi: true,
+          singleQuote: false,
+          trailingComma: "all",
+          printWidth: 80,
+          tabWidth: 2,
+        },
+      ],
+    },
   },
 );
