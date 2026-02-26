43588f160e9d9eb14e60744c8aba3197c7d6cc1e
Erdem Bircan <8729400+erdembircan@users.noreply.github.com>
2026-02-26 19:54:00 +0300

    Implement bin entry point for CLI invocation (#58)
    Add bin/plugin-rescope.js that imports PluginRescope from the built output and wires it up to process.cwd() and process.argv.slice(2), enabling invocation via npx plugin-rescope or as a globally installed binary.\n\nCloses #57

--- bin/plugin-rescope.js
+++ bin/plugin-rescope.js
@@ -1,1 +1,7 @@
 #!/usr/bin/env node
+/* eslint-disable no-undef */
+
+import { PluginRescope } from "../build/index.js";
+
+const rescope = new PluginRescope(process.cwd());
+rescope.rescope(process.argv.slice(2));
