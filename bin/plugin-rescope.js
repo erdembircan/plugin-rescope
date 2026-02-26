4edd60ac34dcef04408ac519b2fb4abe25f208da
Erdem Bircan <erdem.erdembircan@gmail.com>
2026-02-26 19:25:39 +0300

    feat: replace tsc + terser build pipeline with esbuild
    Switch to esbuild for bundling so that path aliases (#core/*, #util/*)
    are resolved at build time, producing a single bundled ESM file.

--- bin/plugin-rescope.js
+++ bin/plugin-rescope.js
@@ -1,1 +1,5 @@
 #!/usr/bin/env node
+import { PluginRescope } from "../build/index.js";
+
+const rescope = new PluginRescope(process.cwd());
+rescope.rescope(process.argv.slice(2));
---
+++ esbuild.config.js
@@ -0,0 +1,12 @@
+import { build } from "esbuild";
+
+await build({
+  entryPoints: ["src/index.ts"],
+  bundle: true,
+  platform: "node",
+  target: "node18",
+  format: "esm",
+  outfile: "build/index.js",
+  minify: true,
+  sourcemap: true,
+});
